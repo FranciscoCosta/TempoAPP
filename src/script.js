c83631ef65af0ba5f293996ea27edb88eb2bbcdf
Francisco Costa <francisco100eg@gmail.com>
2022-08-15 22:39:36 -0300

    mundaça de http para https

--- src/script.js
+++ src/script.js
@@ -17,20 +17,6 @@ btnSearch.addEventListener('click',async()=>{
 })
 
 
-
-const buscaApi = async(cidade)=>{
-    try{
-        const chave = '1b83ba0a5e8b9652e01ec52f0f1dd40d'
-        return fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=5&appid=${chave}`)
-        .then(resposta=>resposta.json())
-    }catch (error){
-        return (error)
-    }
-}
-
-
-
-
 busca.addEventListener('keyup',async(event)=>{
     if((event.key == "Enter")){
     const localidade = busca.value;
